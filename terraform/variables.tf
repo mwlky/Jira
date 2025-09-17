eaba4250d294336851c1652aa363e200de6bb2ae
Damian <mwlky@getgoogleoff.me>
2025-09-17 16:37:47 +0200

    Added base terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  type        = string
+  description = "Location of resource"
+  default     = "West Europe"
+}
+
+variable "rg_name" {
+  type        = string
+  description = "Name of resource group"
+  default     = "JiraRG"
+}
